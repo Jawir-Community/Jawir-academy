448d09cf11684ce3a8830c5adbd6c64b3d6da593
erickdb <erickdarmawanboeniarto130803@gmail.com>
2024-04-10 18:11:40 +0700

    add : temporary navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,25 +1,23 @@
+import { Link } from "react-router-dom";
+import Logo from "../assets/images/jawir.png";
+
 export default function Navbar() {
     return (
       <>
-        <nav className="container mx-auto">
-          <div className="navbar bg-base-100">
-            <div className="flex-1">
-              <a className="btn btn-ghost text-xl">daisyUI</a>
+        <nav className="shadow-md">
+          <div className="navbar container mx-auto  bg-base-100">
+            <div className="navbar-start flex-1">
+              <img className="w-[4.5rem]" src={Logo} alt="" />
             </div>
-            <div className="flex-none">
-              <ul className="menu menu-horizontal px-1">
-                <li><a>Link</a></li>
-                <li>
-                  <details>
-                    <summary>
-                      Parent
-                    </summary>
-                    <ul className="p-2 bg-base-100 rounded-t-none">
-                      <li><a>Link 1</a></li>
-                      <li><a>Link 2</a></li>
-                    </ul>
-                  </details>
-                </li>
+            <div className="navbar-center flex-none">
+              <ul className="menu menu-horizontal px-1 text-xl gap-2">
+                <li><Link to={"/"}>Home</Link></li>
+                <li><Link to={"/about"}>about</Link></li>
+              </ul>
+            </div>
+            <div className="navbar-end flex-1">
+              <ul className="menu menu-horizontal px-1 text-xl">
+                <li className="btn btn-outline"><a href="https://discord.gg/jawir" target="_blank">Join Discord</a></li>
               </ul>
             </div>
           </div>
