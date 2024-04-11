c99542f5c4b5dbd8f1d6cd8edb6ff3c09d62566e
Anuraaaa <anwarfauzan75@gmail.com>
2024-04-11 12:54:42 +0700

    fix: consisten fileName

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter ,Routes ,Route } from 'react-router-dom'
 import React from 'react';
-import Navbar from './components/compNav'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 
