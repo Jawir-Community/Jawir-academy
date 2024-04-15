7b827f689e60693b6e599dd23aff1931df83cb34
erickdb <erickdarmawanboeniarto130803@gmail.com>
2024-04-15 23:01:10 +0700

    refactor responsive contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,11 +2,11 @@ import FormInput from '../components/FormInput';
 export default function Contact() {
     return (
         <>
-            <div className="container mx-auto h-screen flex flex-col justify-center items-center md:items-stretch pl-0 md:pl-10 py-[32rem] sm:py[40rem] md:py-0">
+            <div className="container mx-auto h-screen flex flex-col justify-center px-8 md:pl-10 py-[32rem] sm:py-[40rem] md:py-0">
                 <h2 className="text-5xl sm:text-6xl md:text-8xl mb-10 font-bold">Contact Us</h2>
                 <p className="text-md sm:text-2xl hidden md:block w-[800px] mb-10">We cant solve your problem if you dont tell us about it</p>
                 <div className="flex flex-col md:flex-row justify-start items-start gap-10">
-                    <div className='w-auto md:w-[66%] flex flex-col justify-center'>
+                    <div className='w-full md:w-[66%] flex flex-col justify-center'>
                         <div className="flex flex-col gap-5">
                             <FormInput type="text" svg={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>} placeholder="Your Name" />
                             <FormInput type="text" svg={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>} placeholder="Your Email" />
